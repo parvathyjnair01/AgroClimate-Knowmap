1c8140327811e4b413415d53ed1445e0d38171b6
Adityasingh70309 <adityasingh70309@gmail.com>
2025-11-27 18:47:57 +0530

    Delete Infosys/agrobase directory

--- Infosys/agrobase/pipelines/text_cleaner.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def clean_text(text: str) -> str:
-    if not isinstance(text, str):
-        return ""
-    text = re.sub(r'\s+', ' ', text)
-    text = text.replace('\n', ' ').replace('\r', ' ')
-    return text.strip()
